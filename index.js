f8b97882cfc797b95445f362a13c8a6310c20b5b
Jacob Harvey <jacobharv00@gmail.com>
2021-01-15 04:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if(string === string.toUpperCase()) {
     return 'YES INDEED!'
-  } else {
-    return 'test'
+  } else if(string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  } else{
+    return string 
   }
 }
