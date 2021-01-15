6d81bd2f0b2bf090e6d83cd29e928a593faed2f8
Jacob Harvey <jacobharv00@gmail.com>
2021-01-15 03:06:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+// describe('shout(string)', function shout('hello'){
+//   return'hello'.toUpperCase()
+//   it('receives one argument and returns it in all caps', function() {
+//
+//   })
+// })
+
+//part 1. we need to make a function called shour
+// part 2. we need to give the function an argument
+function shout(taco){
+// part 3. take that argument and uppercase it
+//part 4. return the CAPSED argument
+
+return taco.toUpperCase()
+}
