950b13864e538428a077adb51275a0fb4ec375b4
Jacob Harvey <jacobharv00@gmail.com>
2021-01-15 03:08:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,8 @@
 
 //part 1. we need to make a function called shour
 // part 2. we need to give the function an argument
-function shout(taco){
 // part 3. take that argument and uppercase it
 //part 4. return the CAPSED argument
-
-return taco.toUpperCase()
+function shout(string){
+  return string.toUpperCase()
 }
